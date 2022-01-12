bc8a3c2388368786b519eb9a6fd66c1d400e1c5b
cagatay soyer <cagataysoyer@gmail.com>
2022-01-12 18:29:57 +0300

    persistent vector for gameIDs

--- assembly/model.ts
+++ assembly/model.ts
@@ -1,10 +1,12 @@
-import { RNG, context, PersistentMap, logging, u128 } from 'near-sdk-as';
+import { RNG, context, PersistentMap, logging, u128, PersistentVector } from 'near-sdk-as';
 
 export enum GameState {
   Created,
   InProgress,
   Completed
 }
+
+const MAX_GAMEIDS = 12;
 
 @nearBindgen
 export class GuessMyNumber {
@@ -16,6 +18,7 @@ export class GuessMyNumber {
   roundsPlayed: u8;
   choosedNumber: u8;
   totalAmount: u128;
+  creationAmount: u128;
   bet: u128 = u128.Zero;
 
   constructor() {
@@ -35,7 +38,19 @@ export class GuessMyNumber {
     this.player2 = '';
     this.roundsPlayed = 0;
     this.totalAmount = context.attachedDeposit;
+    this.creationAmount = context.attachedDeposit;
+  }
+
+  static all(): GuessMyNumber[] {
+    const numGameIds = min(MAX_GAMEIDS, gameIds.length);
+    const startIndex = gameIds.length - numGameIds;
+    const result = new Array<GuessMyNumber>(numGameIds);
+    for (let i = 0; i < numGameIds; i++) {
+      result[i] = gameIds[i + startIndex];
+    }
+    return result;
   }
 }
 
 export const games = new PersistentMap<u32, GuessMyNumber>('g');
+export const gameIds = new PersistentVector<GuessMyNumber>('i');
